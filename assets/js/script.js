39d7b826d179cf12b35f8a3bbebbc887443d1986
Ana Patricia da Silva <you@example.com>
2021-08-20 00:41:15 -0300

    Projeto finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,31 +11,70 @@ Por Seletor: querySelec tor()
 let nome = window.document.getElementById("nome")
 let email = document.querySelector("#email")
 let assunto = document.querySelector("#assunto")
+let nomeOK = false
+let emailOK = false
+let assuntoOK = false
+let mapa = document.querySelector("#mapa")
 
-nome.style.width = "100%"  
-email.style.width = "100%"
+nome.style.width = "98%"  
+email.style.width = "98%"
 
 function validaNome(){
      let textNome = document.querySelector("#txtNome")
-    if (nome.value.length < 3) {
+    
+     if (nome.value.length < 3) {
       textNome.innerHTML = "Nome Inválido" 
       textNome.style.color ="red"
     } else {
         textNome.innerHTML ="Nome Válido"
         textNome.style.color = "blue"
+         nomeOK = true 
     }
 }
 
 function validaEmail(){
     let textEmail = document.querySelector("#txtEmail")
+   
     if (email.value.indexOf("@") == -1||email.value.indexOf(".") == -1) {
       textEmail.innerHTML = "E-mail Inválido" 
       textEmail.style.color ="red"
     } else {
         textEmail.innerHTML ="E-mail Válido"
-        textEmail.style.color = "purple"
+        textEmail.style.color = "blue"
+        emailOK = true
     }
 }
 
+function validaAssunto(){
+  let textAssunto = document.querySelector("#txtAssunto")
+  
+  if (assunto.value.length >= 100) {
+    textAssunto.innerHTML = 
+     "Texto é muito grande, digite no máximo 100 caracteres"
+    textAssunto.style.color = "red"
+    textAssunto.style.display = "block"
+  } else {
+    textAssunto.style.display = "none"
+    assuntoOK = true
+  }
+
+}
+  
+function enviar() {
+  if (nomeOK == true && emailOK == true && assuntoOK == true){
+     alert ("Formulário enviado com sucesso")
+  } else {
+     alert ("Preencha o formulário corretamente antes de envair...")
+  }
+}
+function mapaZoom() {
+  mapa.style.whidth = "800px"
+  mapa.style.height = "600px"
+}
+function mapaNormal() {
+  mapa.style.whidth = "400px"
+  mapa.style.height = "250px"
+}
 
 
+  
